5228470c5825e9505fe43830c9f0d586776d0261
Michael Craig <mcraig>
2021-06-24 10:58:06 -0600

    changed spacing

--- UCSB/js/prm-brief-result-container-after.js
+++ UCSB/js/prm-brief-result-container-after.js
@@ -5,45 +5,42 @@
  * 
 **/
 (function () {
-    angular.module('viewCustom')
-    .controller('prmBriefResultContainerAfterCtrl',['$location','$scope',function ($location,$scope) {
-        var vm=this;
-        var param=$location.search();
-        vm.cssClass='marginLeftFindingAid';
-        vm.findingAid={'displayLabel':'','linkURL':'','newLinkURL':''};
-        vm.$onInit=()=>{
-            // get links data from primo parent-ctrl binding data
-            $scope.$watch('vm.parentCtrl.links',()=>{
-                // find $$Elinktofa
-                if(vm.parentCtrl.links) {
-                    for(var i=0; i < vm.parentCtrl.links.length; i++) {
-                        var linkItem=vm.parentCtrl.links[i];
-                        var falink = '';
-                        if(linkItem.displayLabel === 'Finding aid') {
-                            vm.findingAid = linkItem;
-                            if(linkItem.linkURL){
-								                falink = linkItem.linkURL;
-                            }
-                            vm.findingAid.newLinkURL = falink;
-                            i = vm.parentCtrl.links.length;
-                        }
-                    }
-                }
-            });
-            // add more padding when it is full display page
-            if(param.docid) {
-                vm.cssClass='marginLeftFindingAid2';
+  angular.module('viewCustom')
+  .controller('prmBriefResultContainerAfterCtrl',['$location','$scope',function ($location,$scope) {
+    var vm=this;
+    var param=$location.search();
+    vm.cssClass='marginLeftFindingAid';
+    vm.findingAid={'displayLabel':'','linkURL':'','newLinkURL':''};
+    vm.$onInit=()=>{
+      // get links data from primo parent-ctrl binding data
+      $scope.$watch('vm.parentCtrl.links',()=>{
+        // find $$Elinktofa
+        if(vm.parentCtrl.links) {
+          for(var i=0; i < vm.parentCtrl.links.length; i++) {
+            var linkItem=vm.parentCtrl.links[i];
+            var falink = '';
+            if(linkItem.displayLabel === 'Finding aid') {
+              vm.findingAid = linkItem;
+              if(linkItem.linkURL){
+                falink = linkItem.linkURL;
+              }
+              vm.findingAid.newLinkURL = falink;
+              i = vm.parentCtrl.links.length;
             }
-        };
-
-    }]);
-
-    angular.module('viewCustom')
-    .component('prmBriefResultContainerAfter',{
-        bindings:{parentCtrl:'<'},
-        controller: 'prmBriefResultContainerAfterCtrl',
-        controllerAs:'vm',
-        templateUrl:'custom/01UCSB_INST-UCSB/html/prm-brief-result-container-after.html'
-    });
-
+          }
+        }
+      });
+      // add more padding when it is full display page
+      if(param.docid) {
+        vm.cssClass='marginLeftFindingAid2';
+      }
+    };
+  }]);
+  angular.module('viewCustom')
+  .component('prmBriefResultContainerAfter',{
+    bindings:{parentCtrl:'<'},
+    controller: 'prmBriefResultContainerAfterCtrl',
+    controllerAs:'vm',
+    templateUrl:'custom/01UCSB_INST-UCSB/html/prm-brief-result-container-after.html'
+  });
 })();
