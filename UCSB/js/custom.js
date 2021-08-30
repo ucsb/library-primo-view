83a072d7058f745a7a169d359326ee4d8b2d879b
Michael Craig <mcraig>
2021-08-30 14:22:50 -0600

    updated aggregate file

--- UCSB/js/custom.js
+++ UCSB/js/custom.js
@@ -19,7 +19,7 @@
    * Code courtesy of UCI
    * 
    * ***/
-  /*
+  /**
   var alertBanner = window.setInterval(function () {
     var date = new Date();
     if (date.getFullYear() == 2021 && date.getMonth() < 8) {
@@ -48,7 +48,7 @@
       clearInterval(alertBanner);
     }
   }, 5000);
-  */
+  **/
   // Adds the chat button
   (function () {
     var s = document.createElement('script');
@@ -150,112 +150,6 @@
     bindings: { parentCtrl: '<' },
     controller: 'SearchBarAfterController',
     templateUrl: 'custom/01UCSB_INST-UCSB/html/ucls-logo.html'
-  });
-  app.value('searchTargets', [{
-    "name": "Worldcat",
-    "url": "https://ucsb.on.worldcat.org/v2/search?",
-    "img": "custom/01UCSB_INST-UCSB/img/worldcat.png",
-    "alt": "Worldcat logo",
-    mapping: function mapping(queries, filters) {
-      var query_mappings = {
-        'any': 'kw',
-        'title': 'ti',
-        'creator': 'au',
-        'subject': 'su',
-        'isbn': 'bn',
-        'issn': 'n2'
-      };
-      try {
-        return 'queryString=' + queries.map(function (part) {
-          var terms = part.split(',');
-          var type = query_mappings[terms[0]] || 'kw';
-          var string = terms[2] || '';
-          var join = terms[3] || '';
-          return type + ':' + string + ' ' + join + ' ';
-        }).join('');
-      } catch (e) {
-        return '';
-      }
-    }
-  }, {
-    "name": "Google Scholar",
-    "url": "https://scholar.google.com/scholar?q=",
-    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/200px-Google_%22G%22_Logo.svg.png",
-    "alt": "Google Scholar Logo",
-    mapping: function mapping(queries, filters) {
-      try {
-        return queries.map(function (part) {
-          return part.split(",")[2] || "";
-        }).join(' ');
-      } catch (e) {
-        return '';
-      }
-    }
-  }]);
-  /*
-   * From https://github.com/alliance-pcsg/primo-explore-external-search
-   *
-   * With customizations, all commented below.
-   */
-  angular.module('externalSearch', []).value('searchTargets', []).component('prmFacetAfter', {
-    bindings: { parentCtrl: '<' },
-    controller: ['externalSearchService', function (externalSearchService) {
-      externalSearchService.controller = this.parentCtrl;
-      externalSearchService.addExtSearch();
-    }]
-  }).component('prmPageNavMenuAfter', {
-    controller: ['externalSearchService', function (externalSearchService) {
-      if (externalSearchService.controller) externalSearchService.addExtSearch();
-    }]
-  }).component('prmFacetExactAfter', {
-    bindings: { parentCtrl: '<' },
-    template: '\n      <div ng-if="name === \'External Search\'">\n        <div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">\n          <div class="section-content animate-max-height-variable" id="external-search">\n            <div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">\n              <div id="external-search-item" class="md-chip-content layout-row" role="button" tabindex="0">\n                <strong dir="auto" title="{{ target.name }}">\n                  <a ng-href="{{ target.url + target.mapping(queries, filters) }}" target="_blank">\n                    <img ng-src="{{ target.img }}" width="20" height="20"/> {{ target.name }}\n                  </a>\n                  <span class="desc">{{target.desc}}</span>\n                </strong>\n              </div>\n            </div>\n          </div>\n        </div>\n      </div>',
-    controller: ['$scope', '$location', 'searchTargets', function ($scope, $location, searchTargets) {
-      $scope.name = this.parentCtrl.facetGroup.name;
-      $scope.targets = searchTargets;
-      var query = $location.search().query;
-      var filter = $location.search().pfilter;
-      $scope.queries = Array.isArray(query) ? query : query ? [query] : false;
-      $scope.filters = Array.isArray(filter) ? filter : filter ? [filter] : false;
-    }]
-  }).factory('externalSearchService', function () {
-    return {
-      get controller() {
-        return this.prmFacetCtrl || false;
-      },
-      set controller(controller) {
-        this.prmFacetCtrl = controller;
-      },
-      addExtSearch: function addExtSearch() {
-        if (this.prmFacetCtrl.$stateParams.search_scope != 'CourseReserves') {
-          var xx = this;
-          if (xx.prmFacetCtrl.$stateParams.search_scope == 'wcat_profile') {
-            xx.prmFacetCtrl.facetService.results.unshift({
-              name: 'External Search',
-              displayedType: 'exact',
-              limitCount: 0,
-              facetGroupCollapsed: false,
-              values: undefined
-            });
-          } else {
-            var checkExist = setInterval(function () {
-              if (xx.prmFacetCtrl.facetService.results[0] && xx.prmFacetCtrl.facetService.results[0].name != "External Search") {
-                if (xx.prmFacetCtrl.facetService.results.name != "External Search") {
-                  xx.prmFacetCtrl.facetService.results.unshift({
-                    name: 'External Search',
-                    displayedType: 'exact',
-                    limitCount: 0,
-                    facetGroupCollapsed: false,
-                    values: undefined
-                  });
-                }
-                clearInterval(checkExist);
-              }
-            }, 50);
-          }
-        }
-      }
-    };
   });
   /***
   * 
@@ -377,6 +271,7 @@
       return value.match(/^(\(ocolc\))?\d+$/i);
     };
   
+    // Change text based on copyright status of resource
     var isProtected = function isProtected(rightsCode) {
       switch (rightsCode) {
         case 'ic':
@@ -442,5 +337,112 @@
             <prm-icon external-link="" icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>\
           </a>\
         </span>'
+  });
+  
+  app.value('searchTargets', [{
+    "name": "Worldcat",
+    "url": "https://ucsb.on.worldcat.org/v2/search?",
+    "img": "custom/01UCSB_INST-UCSB/img/worldcat.png",
+    "alt": "Worldcat logo",
+    mapping: function mapping(queries, filters) {
+      var query_mappings = {
+        'any': 'kw',
+        'title': 'ti',
+        'creator': 'au',
+        'subject': 'su',
+        'isbn': 'bn',
+        'issn': 'n2'
+      };
+      try {
+        return 'queryString=' + queries.map(function (part) {
+          var terms = part.split(',');
+          var type = query_mappings[terms[0]] || 'kw';
+          var string = terms[2] || '';
+          var join = terms[3] || '';
+          return type + ':' + string + ' ' + join + ' ';
+        }).join('');
+      } catch (e) {
+        return '';
+      }
+    }
+  }, {
+    "name": "Google Scholar",
+    "url": "https://scholar.google.com/scholar?q=",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/200px-Google_%22G%22_Logo.svg.png",
+    "alt": "Google Scholar Logo",
+    mapping: function mapping(queries, filters) {
+      try {
+        return queries.map(function (part) {
+          return part.split(",")[2] || "";
+        }).join(' ');
+      } catch (e) {
+        return '';
+      }
+    }
+  }]);
+  /*
+   * From https://github.com/alliance-pcsg/primo-explore-external-search
+   *
+   * With customizations, all commented below.
+   */
+  angular.module('externalSearch', []).value('searchTargets', []).component('prmFacetAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: ['externalSearchService', function (externalSearchService) {
+      externalSearchService.controller = this.parentCtrl;
+      externalSearchService.addExtSearch();
+    }]
+  }).component('prmPageNavMenuAfter', {
+    controller: ['externalSearchService', function (externalSearchService) {
+      if (externalSearchService.controller) externalSearchService.addExtSearch();
+    }]
+  }).component('prmFacetExactAfter', {
+    bindings: { parentCtrl: '<' },
+    template: '\n      <div ng-if="name === \'External Search\'">\n        <div ng-hide="$ctrl.parentCtrl.facetGroup.facetGroupCollapsed">\n          <div class="section-content animate-max-height-variable" id="external-search">\n            <div ng-repeat="target in targets" aria-live="polite" class="md-chip animate-opacity-and-scale facet-element-marker-local4">\n              <div id="external-search-item" class="md-chip-content layout-row" role="button" tabindex="0">\n                <strong dir="auto" title="{{ target.name }}">\n                  <a ng-href="{{ target.url + target.mapping(queries, filters) }}" target="_blank">\n                    <img ng-src="{{ target.img }}" width="20" height="20"/> {{ target.name }}\n                  </a>\n                  <span class="desc">{{target.desc}}</span>\n                </strong>\n              </div>\n            </div>\n          </div>\n        </div>\n      </div>',
+    controller: ['$scope', '$location', 'searchTargets', function ($scope, $location, searchTargets) {
+      $scope.name = this.parentCtrl.facetGroup.name;
+      $scope.targets = searchTargets;
+      var query = $location.search().query;
+      var filter = $location.search().pfilter;
+      $scope.queries = Array.isArray(query) ? query : query ? [query] : false;
+      $scope.filters = Array.isArray(filter) ? filter : filter ? [filter] : false;
+    }]
+  }).factory('externalSearchService', function () {
+    return {
+      get controller() {
+        return this.prmFacetCtrl || false;
+      },
+      set controller(controller) {
+        this.prmFacetCtrl = controller;
+      },
+      addExtSearch: function addExtSearch() {
+        if (this.prmFacetCtrl.$stateParams.search_scope != 'CourseReserves') {
+          var xx = this;
+          if (xx.prmFacetCtrl.$stateParams.search_scope == 'wcat_profile') {
+            xx.prmFacetCtrl.facetService.results.unshift({
+              name: 'External Search',
+              displayedType: 'exact',
+              limitCount: 0,
+              facetGroupCollapsed: false,
+              values: undefined
+            });
+          } else {
+            var checkExist = setInterval(function () {
+              if (xx.prmFacetCtrl.facetService.results[0] && xx.prmFacetCtrl.facetService.results[0].name != "External Search") {
+                if (xx.prmFacetCtrl.facetService.results.name != "External Search") {
+                  xx.prmFacetCtrl.facetService.results.unshift({
+                    name: 'External Search',
+                    displayedType: 'exact',
+                    limitCount: 0,
+                    facetGroupCollapsed: false,
+                    values: undefined
+                  });
+                }
+                clearInterval(checkExist);
+              }
+            }, 50);
+          }
+        }
+      }
+    };
   });
 })();
