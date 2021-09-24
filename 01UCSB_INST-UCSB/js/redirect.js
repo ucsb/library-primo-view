d8b6f7dbffade60976903914831a593bb6ad79df
Michael Craig <mcraig>
2021-09-24 14:08:31 -0600

    added redirect checks for old domains

--- 01UCSB_INST-UCSB/js/redirect.js
+++ 01UCSB_INST-UCSB/js/redirect.js
@@ -1,14 +1,14 @@
 /**
-	Redirect users from old primo's url to new url.
+	Redirect users from undesired domains to branded domain for primo.
 **/
 var jQueryScript = document.createElement("script");
 jQueryScript.src = "https://code.jquery.com/jquery-3.3.1.min.js";
 document.getElementsByTagName("head")[0].appendChild(jQueryScript);
 
-
 jQueryScript.onload = function() {
 	$(document).ready(function(){
-		if (window.location.host === "ucsb-primo.hosted.exlibrisgroup.com" ) {
+    let domains = ["ucsb-primo.hosted.exlibrisgroup.com", "na07.primo.exlibrisgroup.com", "ucsb.primo.exlibrisgroup.com"];
+		if (domains.includes(window.location.host)) {
 			var oldPath = window.location.pathname + window.location.search;
 			window.location.replace("https://search.library.ucsb.edu" + oldPath);
 	  }
