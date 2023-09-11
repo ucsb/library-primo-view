f8f4b2344338b80529916c1052d6afa9e571a4d3
Michael Craig <mscraig@ucsb.edu>
2023-09-11 10:03:54 -0700

    add hover to banner and fix date condition

--- js/alertBar.js
+++ js/alertBar.js
@@ -4,7 +4,7 @@
  */
 var alertBanner = window.setInterval(function () {
   var date = new Date();
-  if (date.getFullYear() == 2023 && date.getMonth() > 8  && date.getMonth() < 11) {
+  if (date.getFullYear() == 2023 && date.getMonth() > 7  && date.getMonth() < 10) {
     var prmAlertBar = document.getElementsByClassName('topbar-wrapper');
     var alertBarDiv = document.createElement('div');
     alertBarDiv.setAttribute('id', 'customAlertBar');
@@ -12,13 +12,13 @@ var alertBanner = window.setInterval(function () {
     alertBarDiv.setAttribute('layout-align', 'center center');
     var alertBarInnerDiv = document.createElement('div');
 
-    if (date.getMonth() == 9 && date.getDate() > 10 && date.getDate() < 23) {
+    if ((date.getMonth() == 8 && date.getDate() > 10 && date.getDate() < 23) || (date.getMonth() == 9)) {
       alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #FFCE34;padding: 3px 0;font-size: 15px; color: #000000; font-family: Source Sans Pro,Helvetica Neue,Helvetica,Arial,sans-serif;');
-      alertBarInnerDiv.innerHTML = 'September 18-22, 2023: Art & Architecture General Collection print materials with call numbers N-NC will be unavailable while they are relocated to the 2nd floor, Mountain Side.<br> <a href="https://www.library.ucsb.edu/arts-library-project">Click here</a> to learn more';
+        alertBarInnerDiv.innerHTML = 'September 18-22, 2023: Art & Architecture General Collection print materials with call numbers N-NC will be unavailable while they are relocated to the <a class="alertBarHoverImg" href="https://www.library.ucsb.edu/files/artslibraryprojectn-nchighlightedpng">2nd floor, Mountain Side<span><img src="custom/01UCSB_INST-UCSB/img/bannerMap.png" alt="second floor map" width="40%" height="auto" /></span></a>.<br> To learn more, <a href="https://www.library.ucsb.edu/arts-library-project">click here</a>.';
     }
     else if ((date.getMonth() == 9 && date.getDate() > 22) || (date.getMonth() == 10 && date.getDate() < 14)) {
       alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #FFCE34;padding: 3px 0;font-size: 15px; color: #000000; font-family: Source Sans Pro,Helvetica Neue,Helvetica,Arial,sans-serif;');
-      alertBarInnerDiv.innerHTML = 'Art & Architecture General Collection print materials with call numbers N-NC are temporarily located on the 2nd floor, Mountain Side.<br> <a href="https://www.library.ucsb.edu/arts-library-project">Click here</a> to learn more.';
+      alertBarInnerDiv.innerHTML = 'Art & Architecture General Collection print materials with call numbers N-NC are temporarily located on the <a class="alertBarHoverImg" href="https://www.library.ucsb.edu/files/artslibraryprojectn-nchighlightedpng">2nd floor, Mountain Side<span><img src="custom/01UCSB_INST-UCSB/img/bannerMap.png" alt="second floor map" width="40%" height="auto" /></span></a>.<br> To learn more, <a href="https://www.library.ucsb.edu/arts-library-project">click here</a>.';
     }
     alertBarDiv.appendChild(alertBarInnerDiv);
     prmAlertBar[0].prepend(alertBarDiv);
